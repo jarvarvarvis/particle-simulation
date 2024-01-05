94f174f3dfbf2634b516c57f7bcf3d357b9e1bcc
jarvarvarvis <jarvistrigo@gmail.com>
2024-01-05 14:49:49 +0100

    Ignore non-significant OpenGL error/warning codes in debug message handler

--- src/opengl/debug.c
+++ src/opengl/debug.c
@@ -32,6 +32,11 @@ const char *debug_get_err_type_name(GLenum type) {
 void debug_message(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length,
                   const GLchar *message, const void *user_param)
 {
+    // Ignore non-significant error/warning codes
+    if (id == 131169 || id == 131185 || id == 131218 || id == 131204) {
+        return;
+    }
+
     CLogSeverity log_severity;
     if (severity == GL_DEBUG_SEVERITY_HIGH)         log_severity = C_LOG_SEVERITY_ERROR;
     if (severity == GL_DEBUG_SEVERITY_MEDIUM)       log_severity = C_LOG_SEVERITY_WARNING;
